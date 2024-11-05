98c4f164c1130bf6ef91c1cd4efb202636484e4a
Yorutale <amir.zhumabek.02@icloud.com>
2024-11-05 13:22:09 +0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+var slicesPerPizza = 8;
+var numberOfPizza = 5;
+var totalSlices = sclicesPerPizza * numberOfPizza;
+
+console.log(totalSlices);
+
+var totalChocolate = 24;
+var numberOfFriends = 6;
+var chocolatesPerFriend = totalChocolate / numberOfFriends;
+
+console.log(chocolatesPerFriend);
+
+var plantHeight = 10;
+plantHeight += 2;
+
+console.log(plantHeight);
